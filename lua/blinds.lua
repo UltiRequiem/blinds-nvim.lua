52f0be63cc930f4004a89b800b7be6b43cddd120
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 22:31:28 -0500

    :art: Update

--- lua/blinds.lua
+++ lua/blinds.lua
@@ -10,9 +10,10 @@ function! s:SetBlinds()
 endfun
 
 augroup blinds
-autocmd WinEnter,BufWinEnter * setlocal winhighlight=
-autocmd WinLeave * if &bl == 1 | setlocal winhighlight=ColorColumn:Blinds,CursorColumn:Blinds,CursorLine:Blinds,EndOfBuffer:Blinds,LineNr:Blinds,NonText:Blinds,Normal:Blinds,FoldColumn:Blinds,SignColumn:Blinds,VertSplit:Blinds,Whitespace:Blinds | endif
-autocmd ColorScheme * call s:SetBlinds()
+  autocmd!
+  autocmd WinEnter,BufWinEnter * setlocal winhighlight=
+  autocmd WinLeave * if &bl == 1 | setlocal winhighlight=ColorColumn:Blinds,CursorColumn:Blinds,CursorLine:Blinds,EndOfBuffer:Blinds,LineNr:Blinds,NonText:Blinds,Normal:Blinds,FoldColumn:Blinds,SignColumn:Blinds,VertSplit:Blinds,Whitespace:Blinds | endif
+  autocmd ColorScheme * call s:SetBlinds()
 augroup END
 
 call s:SetBlinds()
