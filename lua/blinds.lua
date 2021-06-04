386fa26c18e293636abe7d51f3547cbc18007b12
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 22:10:16 -0500

    :art: Update

--- lua/blinds.lua
+++ lua/blinds.lua
@@ -1,3 +1,7 @@
 vim.g.blinds_guibg = "#969694"
 
-vim.api.nvim_command("hi Blinds guibg=" .. vim.g.blinds_guibg)
+local function setup()
+  vim.api.nvim_command("hi Blinds guibg=" .. vim.g.blinds_guibg)
+end
+
+return {setup = setup}
