994a1b475b76d29192798c42f9f0e08c8aae060f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 22:00:01 -0500

    :art: Update

--- lua/blinds.lua
+++ lua/blinds.lua
@@ -1,33 +1,29 @@
-local plugin = [[
-if (exists("g:loaded_blinds") && g:loaded_blinds) || &cp
-    finish
-endif
-let g:loaded_blinds= 1
-
+local blinds_guibg = [[
 if !exists("g:blinds_guibg")
     let g:blinds_guibg = "#969694"
 endif
+]]
 
+local set_blinds = ([[
 function! s:SetBlinds()
     exec "hi Blinds guibg=".g:blinds_guibg
 endfun
+]])
 
+local autogroup =
+    [[
 augroup blinds
     au!
     au WinEnter,BufWinEnter * setlocal winhighlight=
     au WinLeave * if &bl == 1 | setlocal winhighlight=ColorColumn:Blinds,CursorColumn:Blinds,CursorLine:Blinds,EndOfBuffer:Blinds,LineNr:Blinds,NonText:Blinds,Normal:Blinds,FoldColumn:Blinds,SignColumn:Blinds,VertSplit:Blinds,Whitespace:Blinds | endif
     au ColorScheme * call s:SetBlinds()
 augroup END
-
-if (exists("g:loaded_blinds") && g:loaded_blinds) || &cp
-    finish
-endif
-let g:loaded_blinds= 1
-
 ]]
 
 local function setup()
-  vim.api.nvim_command(plugin)
+  vim.api.nvim_command(blinds_guibg)
+  vim.api.nvim_command(set_blinds)
+  vim.api.nvim_command(autogroup)
 end
 
 return {setup = setup}
