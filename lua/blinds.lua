f9b81c3562a29f59dc23b9b4b64d26d3e8ff79bb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 22:22:29 -0500

    :art: Update

--- lua/blinds.lua
+++ lua/blinds.lua
@@ -2,17 +2,20 @@ local function setup()
   vim.api.nvim_command([[
   if !exists("g:blinds_guibg")
     let g:blinds_guibg = "#969694"
-  endif
- function! s:SetBlinds()
+endif
+
+function! s:SetBlinds()
     exec "hi Blinds guibg=".g:blinds_guibg
-  endfun
-  
-  augroup blinds
+endfun
+
+augroup blinds
     au!
     au WinEnter,BufWinEnter * setlocal winhighlight=
     au WinLeave * if &bl == 1 | setlocal winhighlight=ColorColumn:Blinds,CursorColumn:Blinds,CursorLine:Blinds,EndOfBuffer:Blinds,LineNr:Blinds,NonText:Blinds,Normal:Blinds,FoldColumn:Blinds,SignColumn:Blinds,VertSplit:Blinds,Whitespace:Blinds | endif
     au ColorScheme * call s:SetBlinds()
-  augroup END
+augroup END
+
+call s:SetBlinds()
   ]]
   )
 end
