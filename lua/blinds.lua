491cb37bc784881a4133d83f947d2cfd068cfe2e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-03 22:33:41 -0500

    :art: Update

--- lua/blinds.lua
+++ lua/blinds.lua
@@ -10,7 +10,6 @@ exec "hi Blinds guibg=".g:blinds_guibg
 endfun
 
 augroup blinds
-autocmd!
 autocmd WinEnter,BufWinEnter * setlocal winhighlight=
 autocmd WinLeave * if &bl == 1 | setlocal winhighlight=ColorColumn:Blinds,CursorColumn:Blinds,CursorLine:Blinds,EndOfBuffer:Blinds,LineNr:Blinds,NonText:Blinds,Normal:Blinds,FoldColumn:Blinds,SignColumn:Blinds,VertSplit:Blinds,Whitespace:Blinds | endif
 autocmd ColorScheme * call s:SetBlinds()
